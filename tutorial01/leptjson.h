1a64e4b713d4e33bbee1c7faf91e44929d2c6f44
MineFall <79210721+MineFall@users.noreply.github.com>
2021-06-03 23:26:10 +0800

    Update leptjson.h

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -12,9 +12,10 @@ enum {
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
     LEPT_PARSE_ROOT_NOT_SINGULAR
-};
+};//枚举类，处理分析的错误分类
 
 int lept_parse(lept_value* v, const char* json);//用于分析的API,  lept_value* v在分析之前没有值，而是在分析之后获得值，也就是保存分析后的结果
+//并且json值不应该被改变，所以需要用const进行修饰
 
 lept_type lept_get_type(const lept_value* v);//如果lept_value取得了值，那么就可以通过这个API获得其中的成员值
 
