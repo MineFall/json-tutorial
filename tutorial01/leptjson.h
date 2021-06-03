0546d6dd6afd34cf810706ffe4c30f11149d9fd0
MineFall <79210721+MineFall@users.noreply.github.com>
2021-06-03 23:23:42 +0800

    Update leptjson.h

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -5,7 +5,7 @@ typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_
 
 typedef struct {
     lept_type type;
-}lept_value;
+}lept_value;//lept_value等于就是这个结构体类的名称，虽然这个结构体是一个无名类
 
 enum {
     LEPT_PARSE_OK = 0,
@@ -14,8 +14,8 @@ enum {
     LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
-int lept_parse(lept_value* v, const char* json);
+int lept_parse(lept_value* v, const char* json);//用于分析的API,  lept_value* v在分析之前没有值，而是在分析之后获得值，也就是保存分析后的结果
 
-lept_type lept_get_type(const lept_value* v);
+lept_type lept_get_type(const lept_value* v);//如果lept_value取得了值，那么就可以通过这个API获得其中的成员值
 
 #endif /* LEPTJSON_H__ */
